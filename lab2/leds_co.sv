924171f2cb03937c667b6c2afc2ee97d199ec429
cojedadesilva <cojedadesilva@g.hmc.edu>
2025-09-15 14:32:38 -0700

    Add files via upload

--- lab2/leds_co.sv
+++ lab2/leds_co.sv
@@ -1,11 +1,13 @@
 /*
-by Carlos Ojeda de Silva, September 10 2025
+by Carlos Ojeda de Silva, September 3 2025
 cojedadesilva@g.hmc.edu
-This file controls which 7 segment display is used with the corresponding s values. It also outputs the sum of 2 numbers in binary with 5 leds
+This file does and and and xor operatioon on 4 switches and also makes a 2.4Hz blinking LED.
 */
 module leds_co(
+	input reset,
 	input logic [3:0] s1, s2,
-	output 	logic [1:0] fpga_blink_out,
+	input logic [31:0] counter, //uncomment for testbench
+	output 	logic [1:0] disps,
 	output logic [3:0] sout,
 	output logic [4:0] leds
 );
@@ -13,20 +15,29 @@ module leds_co(
 	logic int_osc;
 	logic pulse; 
 	logic led_state = 0;
-	logic [31:0] counter = 0;
+	//logic [31:0] counter = 0; //comment for testbench
 	
 	// Internal high-speed oscillator 
 	HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
 	
 	// Simple clock divider
-	
-	always_ff @(posedge int_osc)
+	/*
+	always_ff @(posedge int_osc) //comment for testbench
 		begin
-			counter <= counter + 215; //this value works
+			counter <= counter + 215; 
 			if (counter[21]) sout = s1;
-			if (~counter[21]) sout = s2; //using index 21 makes it around 2.6kHz, which is higher than human eye and slow enough for the transistor.
-			leds = s1 + s2;
-		end
-  assign fpga_blink_out[0] = counter[21];
-  assign fpga_blink_out[1] = ~counter[21];
+			if (~counter[21]) sout = s2;
+			if (reset) counter = 0;
+		end //*/
+  assign leds = s1 + s2;
+  assign disps[0] = counter[21];
+  assign disps[1] = ~counter[21];
+  //*
+  always @(*) begin
+  if (counter[21]) sout = s2; //uncomment for testbench
+  if (~counter[21]) sout = s1;
+  end
+  //*/
+  
+  
 endmodule
--- lab2/top.sv
+++ lab2/top.sv
@@ -1,15 +1,17 @@
 /*
-by Carlos Ojeda de Silva, September 10 2025
+by Carlos Ojeda de Silva, September 3 2025
 cojedadesilva@g.hmc.edu
-This file combines both tasks for lab2
+This file combines both tasks for lab1
 */
 module top(
+	input 	reset,
 	input 	logic [3:0] s1, s2,
-	output 	logic [1:0] fpga_blink_out,
+	input   logic [31:0] counter, //uncomment for testbench
+	output 	logic [1:0] disps,
 	output  logic [6:0] number,
 	output  logic [4:0] leds
 );
 logic [3:0] sout;
-leds_co leds_co(s1[3:0], s2[3:0], fpga_blink_out, sout[3:0], leds[4:0]);
-lab1_co lab1_co(sout[3:0], number[6:0]);
+leds_co leds_co(reset, s1[3:0], s2[3:0],/**/ counter[31:0],/*uncomment for testbench*/ disps, sout[3:0], leds[4:0]);
+sevseg sevseg(sout[3:0], number[6:0]);
 endmodule
